d3bd2b443e313267394ffaaba5a94c36b22e1120
Prudnikova <dakoto4ka@yandex.ru>
2023-05-24 17:15:26 +0300

    Initial commit

---
+++ src/main/java/pro/sky/k2hw10libraries/Application.java
@@ -0,0 +1,13 @@
+package pro.sky.k2hw10libraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/k2hw10libraries/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.k2hw10libraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
